3343f58a43cd45a9276520afe9893db13068229b
Jeisson Chacón <Jeissonchaconjp@gmail.com>
2023-06-21 16:23:03 -0500

    Initial Commit

---
+++ PruebaGit/PruebaGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  PruebaGitApp.swift
+//  PruebaGit
+//
+//  Created by Jeisson Chacón on 6/21/23.
+//
+
+import SwiftUI
+
+@main
+struct PruebaGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
